d1d3412436a1ed20e033c8757afef40728e1b13e
Banu <banumanj@gmail.com>
2020-03-01 22:57:58 +0000

    Adding pet model class files

---
+++ src/main/java/Pet/Pet.java
@@ -0,0 +1,65 @@
+package Pet;
+
+import java.util.List;
+
+public class Pet {
+    private Category category;
+
+    private Long id;
+
+    private String name;
+
+    private List<String> photoUrls;
+
+    private String status;
+
+    private List<Tags> tags;
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getPhotoUrls() {
+        return photoUrls;
+    }
+
+    public void setPhotoUrls(List<String> photoUrls) {
+        this.photoUrls = photoUrls;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<Tags> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tags> tags) {
+        this.tags = tags;
+    }
+}
