dfc52e50387c5967968be43f6ebcab17cfc6c420
Banu <banumanj@gmail.com>
2020-03-02 00:52:38 +0000

    resource ad runner file

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,64 @@
+package runner;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.CucumberFeatureWrapper;
+import cucumber.api.testng.TestNGCucumberRunner;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"steps."},
+        tags = {"~@Ignore"},
+        format = {
+                "pretty",
+                "html:target/cucumber-reports/cucumber-pretty",
+                "json:target/cucumber-reports/CucumberTestReport.json",
+                "rerun:target/cucumber-reports/rerun.txt"
+        },plugin = "json:target/cucumber-reports/CucumberTestReport.json")
+
+public class TestRunner {
+    private TestNGCucumberRunner testNGCucumberRunner;
+    public WireMockServer wireMockServer;
+    @BeforeClass(alwaysRun = true)
+    public void setUpClass() throws Exception {
+        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+        setup();
+    }
+
+    @Test(groups = "cucumber", description = "Runs Cucumber Feature", dataProvider = "features")
+    public void feature(CucumberFeatureWrapper cucumberFeature) {
+        testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
+    }
+    @DataProvider
+    public Object[][] features() {
+        return testNGCucumberRunner.provideFeatures();
+    }
+    @AfterClass(alwaysRun = true)
+    public void tearDownClass() throws Exception {
+        testNGCucumberRunner.finish();
+        teardown();
+    }
+
+    public void setup () {
+        wireMockServer = new WireMockServer(8090);
+        wireMockServer.start();
+        setupStub();
+    }
+
+    public void teardown () {
+        wireMockServer.stop();
+    }
+    public void setupStub() {
+        wireMockServer.stubFor(get(urlEqualTo("/pet/findByStatus"))
+                .willReturn(aResponse().withHeader("Content-Type", "text/plain")
+                        .withStatus(200)
+                        .withBodyFile("json/pets.json")));
+    }
+}
